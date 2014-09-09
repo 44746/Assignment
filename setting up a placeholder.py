24ab19e822e18be9f1b9feefc6a19d8589b61a2a
44746 <44746@longroad.ac.uk>
2014-09-09 12:54:44 +0100

    Setting up a placeholder

---
+++ setting up a placeholder.py
@@ -0,0 +1,12 @@
+#George west
+#9/9/14
+#Setting up a placeholder
+
+print("hello world")
+#this is telling the computer to output the string that is between the brackets.
+first_name = input("please enter your name:")
+#this is outputing the string between the brackets and asking the user to input their name.
+print(first_name)
+#this will output the infromation stored under the variable called first_name
+print( "Hi {0}!".format (first_name))
+#this will retrieve the infromation and output it using a placeholder.
