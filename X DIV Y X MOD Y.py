9099f09132b8f3bf09d7b9bfddfbc397467ce1e6
44746 <44746@longroad.ac.uk>
2014-09-12 14:07:33 +0100

    X DIV Y X MOD Y Exercise

---
+++ X DIV Y X MOD Y.py
@@ -0,0 +1,9 @@
+#George West
+#X DIV Y X MOD Y
+#14-09-12
+
+number1= float(input("Please input your first number: "))
+number2= float(input("Please input your second number: "))
+div = number1//number2
+mod = number1%number2
+print("The whole number part of your division is {0} and the remainder is {1}".format(div, mod))
