46e61b15c717fc6ddfa135814357252b549bd6c6
44746 <44746@longroad.ac.uk>
2014-09-12 13:41:15 +0100

    Exercises 12-9-14

---
+++ total score exercise.py
@@ -0,0 +1,7 @@
+#George West
+#12-09-2014
+#Total score exercise
+first_score = int(input("Please enter your first score: "))
+second_score = int(input("Please enter your second score: "))
+total_score = first_score+second_score
+print("Your total score is {0}".format(total_score))
