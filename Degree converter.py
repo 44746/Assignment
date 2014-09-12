f754603165157b2f3860d0e930c490126a04534c
44746 <44746@longroad.ac.uk>
2014-09-12 14:14:33 +0100

    Degree converter

---
+++ Degree converter.py
@@ -0,0 +1,8 @@
+#George West
+#12-09-2014
+#Degree converter
+
+fahren = float(input("Please input the temperature in fahrenheit; "))
+centi = (fahren - 32) * (5/9)
+
+print("The temperature in centigrade is {0}".format(centi))
